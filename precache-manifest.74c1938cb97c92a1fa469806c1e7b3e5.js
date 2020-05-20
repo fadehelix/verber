71a70d8528fd0ce12a6df58e99f3f7ef38b29217
Adam Dziendziel <218922+fadehelix@users.noreply.github.com>
2020-05-20 20:52:30 +0200

    Updates

---
+++ precache-manifest.74c1938cb97c92a1fa469806c1e7b3e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3239eadcb09d3df3a4fcbb5c8d79b123",
+    "url": "/verber/index.html"
+  },
+  {
+    "revision": "038ebde4c096848fdfaa",
+    "url": "/verber/static/css/main.1997aa82.chunk.css"
+  },
+  {
+    "revision": "510c4fb26b012f7a5416",
+    "url": "/verber/static/js/2.53f2e485.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/verber/static/js/2.53f2e485.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "038ebde4c096848fdfaa",
+    "url": "/verber/static/js/main.2c98e8aa.chunk.js"
+  },
+  {
+    "revision": "85121df12e264851fb1e",
+    "url": "/verber/static/js/runtime-main.69ff8d82.js"
+  }
+]);
