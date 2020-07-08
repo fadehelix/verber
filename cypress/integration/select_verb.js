c737f03122d66c7a1297a2d3beed8b14c49d0e15
Adam D <fadehelix@gmail.com>
2020-07-08 13:17:48 +0200

    Slightly refactor select_verb.spec

--- cypress/integration/select_verb.js
+++ cypress/integration/select_verb.js
@@ -1,13 +1,13 @@
 /// <reference types="cypress" />
 
-context('Select existing word from autocomplete list', () => {
+context('Type first form of verb', () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit('/');
+    cy.get('#questionInput').type('be').wait(1000);
+    
   })
 
   it('Answer correctly all forms', () => {
-    cy.get('#questionInput').type('be').wait(500);
-
     cy.get('#firstAnswer').as('firstAnswer').type('was').blur();
     cy.get('@firstAnswer').parent().should('have.class', 'answer--isCorrect')
     cy.get('#secondAnswer').type('been').blur().as('secondAnswer');
@@ -15,8 +15,6 @@ context('Select existing word from autocomplete list', () => {
   })
 
   it('Answer incorrectly to second form', () => {
-    cy.get('#questionInput').type('be');
-
     cy.get('#firstAnswer').as('firstAnswer').type('what').blur();
     cy.get('@firstAnswer').parent().should('have.class', 'answer--isIncorrect')
   })
